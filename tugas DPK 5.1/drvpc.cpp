993b375e33f6a75ed43284ded318cee0dd32677b
Hellyoshaqiqie <77888513+hellyoshaqiqie17@users.noreply.github.com>
2023-06-04 11:44:24 +0700

    Update drvpc.cpp

--- tugas DPK 5.1/drvpc.cpp
+++ tugas DPK 5.1/drvpc.cpp
@@ -1,21 +1,40 @@
 #include "pc.h"
 #include <iostream>
-
+ 
 using namespace std;
 
 int main() {
     PulseCounter pc;
     cout << "Masukkan NoKBU: "; cin >> pc.NoKBU;
-    cout << "Masukkan Jam Mulai (HH MM SS): ";
-    int jamMulai, menitMulai, detikMulai; cin >> jamMulai >> menitMulai >> detikMulai;
+
+    cout << "Masukkan Jam Mulai (HH): ";
+    int jamMulai; cin >> jamMulai;
     
-    pc.JamMulai.AssignClock(jamMulai, menitMulai, detikMulai);
-    cout << "Masukkan Jam Selesai (HH MM SS): ";
+    pc.JamMulai.SetJam(jamMulai);
+    cout << "Masukkan Menit Mulai (MM): ";
     
-    int jamSelesai, menitSelesai, detikSelesai; cin >> jamSelesai >> menitSelesai >> detikSelesai;
-    pc.JamSelesai.AssignClock(jamSelesai, menitSelesai, detikSelesai);
+    int menitMulai; cin >> menitMulai;
+    pc.JamMulai.SetMenit(menitMulai);
+    cout << "Masukkan Detik Mulai (SS): ";
     
+    int detikMulai; cin >> detikMulai;
+    pc.JamMulai.SetDetik(detikMulai);
+
+    cout << "Masukkan Jam Selesai (HH): ";
+    int jamSelesai;
+    cin >> jamSelesai;
+    pc.JamSelesai.SetJam(jamSelesai);
+    cout << "Masukkan Menit Selesai (MM): ";
+    
+    int menitSelesai;cin >> menitSelesai;
+    pc.JamSelesai.SetMenit(menitSelesai);
+    cout << "Masukkan Detik Selesai (SS): ";
+    
+    int detikSelesai; cin >> detikSelesai;
+    pc.JamSelesai.SetDetik(detikSelesai);
+
     cout << "Jumlah Pulsa: " << pc.HitungPulsa() << endl;
-    cout << "Biaya Percakapan: " << pc.HitungBiayaPercakapan() << " rupiah" << endl;
+    cout << "Biaya Percakapan: " << pc.HitungBiayaPercakapan() << " rupiah" << std::endl;
+
     return 0;
 }
